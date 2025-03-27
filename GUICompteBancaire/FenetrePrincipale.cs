7560e38bd06ba377ccb094050df26357ec014d7e
Foued <foued.attar@lyceeastier.com>
2025-03-27 15:36:15 +0100

    part 6 3:30;

---
+++ GUICompteBancaire/FenetrePrincipale.cs
@@ -0,0 +1,78 @@
+using CompteBancaireLib;
+
+namespace GUICompteBancaire
+{
+    public partial class FenetrePrincipale : Form
+    {
+        List<GestionCompte> listeComptes = new List<GestionCompte>();
+
+        public FenetrePrincipale()
+        {
+            InitializeComponent();
+            listeComptes.Add(new GestionCompte());
+        }
+
+        private void FenetrePrincipale_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ouvrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Fichiers CSV |*.csv|Fichiers JSON (.json)|*.json|Tous les fichiers(*.*)|*.*";
+            ofd.Multiselect = false;
+            ofd.CheckFileExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        string fichier = ofd.FileName;
+                        string ext = Path.GetExtension(fichier).ToLower();
+                        CompteAvecSauvegarde compte = null;
+                try
+                {
+                    if (ext == ".csv")
+                        {
+                        compte = CompteAvecSauvegarde.LireCSV(fichier);
+                        }
+                        else if (ext == ".json")
+                        {
+                            compte = CompteAvecSauvegarde.LireJSON(fichier);
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Type de fichier inconnu grrr", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+                    bool found = false;
+                    foreach(GestionCompte gc in listeComptes)
+                    {
+                        if(gc.Fichier == fichier)
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        GestionCompte nouvelleFenetre = new GestionCompte();
+                        nouvelleFenetre.Fichier = fichier;
+                        nouvelleFenetre.Compte = compte;
+                        nouvelleFenetre.Show();
+
+                        listeComptes.Add(nouvelleFenetre);
+                    }
+
+                }catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
